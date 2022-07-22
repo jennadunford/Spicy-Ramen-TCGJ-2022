148019b6cdd75a6d50009740774d2d13ca6bbbde
Shen-2102687 <100612118+Shen-2102687@users.noreply.github.com>
2022-07-22 22:46:49 +0200

    added scene manager script

---
+++ Assets/Scripts/sceneManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void goToTemple()
+    {
+        SceneManager.LoadScene("enter name");
+    }
+
+    public void goToForest()
+    {
+        SceneManager.LoadScene("enter name");
+    }
+
+    public void goToWasteLand()
+    {
+        SceneManager.LoadScene("enter name");
+    }
+
+    public void goToSwamp()
+    {
+        SceneManager.LoadScene("enter name");
+    }
+
+    public void goToOverlookLand()
+    {
+        SceneManager.LoadScene("enter name");
+    }
+
+    public void goToChamber()
+    {
+        SceneManager.LoadScene("enter name");
+    }
+}
